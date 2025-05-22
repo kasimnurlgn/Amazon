1ef062cbe336a7c129259f0a8ba77ba53705df6d
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-22 10:25:32 +0300

    carasoul style added

--- src/components/Carousel/CarouselEffect.jsx
+++ src/components/Carousel/CarouselEffect.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import data from "./data";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import css from "./CarouselEffect.module.css";
 const CarouselEffect = () => {
   return (
-    <div>
-      <Carousel>
-        {data.map((image) => (
-          <img src={image} />
+    <>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        interval={2000}
+      >
+        {data.map((image, index) => (
+          <img src={image} key={index} alt={`image ${index + 1}`} />
         ))}
       </Carousel>
-    </div>
+      <div className={css.hero__img}></div>
+    </>
   );
 };
 
