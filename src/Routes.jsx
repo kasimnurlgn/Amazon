dc3d4bf0eccf9e7a0f642c2f09f5c7a39d4875a1
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-24 17:22:41 +0300

    Soem routing errors fixed

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,18 +4,18 @@ import Payment from "./pages/Payment/Payment";
 import Orders from "./pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Auth from "./pages/Auth/Auth";
+import Results from "./pages/Results/Results";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/payments" element={<Payment />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/category/:categoryName" element={<Results />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 };
 
--- src/components/Catagory/CatagoryCard.jsx
+++ src/components/Catagory/CatagoryCard.jsx
@@ -1,15 +1,16 @@
+import { Link } from "@mui/material";
 import css from "./Catagory.module.css";
 const CategoryCard = ({ data }) => {
   console.log(data);
   return (
     <div className={css.catagory}>
-      <a href="">
+      <Link to={`/category/${data.name}`}>
         <span>
           <h2>{data.title}</h2>
         </span>
         <img src={data.imgLink} alt={data.name} />
         <p>Shop now</p>
-      </a>
+      </Link>
     </div>
   );
 };
