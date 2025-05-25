b1f7b76b96d33954d36080428c1e4b7a59c8c2ee
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-25 14:19:09 +0300

    Results page loading functionality added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,21 +2,28 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 import css from "./Product.module.css";
+import Loader from "../Loader/Loader";
 const Product = () => {
   let [products, setProducts] = useState();
+  let [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => {
         setProducts(res.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setIsLoading(false);
       });
   }, []);
-  
-  return (
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <section className={css.products__container}>
       {products?.map((singleProduct) => {
         return <ProductCard product={singleProduct} key={singleProduct.id} />;
--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -5,29 +5,42 @@ import { useParams } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import ProductCard from "../../components/Product/ProductCard";
 import css from "./Results.module.css";
+import Loader from "../../Components/Loader/Loader";
 const Results = () => {
   let [results, setResults] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
   let { categoryName } = useParams();
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`${productUrl}/products/category/${categoryName}`)
-      .then((res) => setResults(res.data))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        setResults(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   console.log(categoryName);
   return (
     <Layout>
-      <section>
-        <h1 style={{ padding: "30px" }}>Results</h1>
-        <p style={{ padding: "30px" }}>Category/ {categoryName}</p>
-        <hr />
-        <div className={css.products__container}>
-          {results?.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </section>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section>
+          <h1 style={{ padding: "30px" }}>Results</h1>
+          <p style={{ padding: "30px" }}>Category/ {categoryName}</p>
+          <hr />
+          <div className={css.products__container}>
+            {results?.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </section>
+      )}
     </Layout>
   );
 };
