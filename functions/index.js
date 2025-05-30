a891636aa68f756f13489fa128d9cfd0f8cbbd57
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-30 09:47:21 +0300

    port handled wiht cors

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
-
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
 
+app.use(cors({ origin: true }));
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "success",
