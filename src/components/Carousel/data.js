6684351f8867136c7a6187ff885accd3a5e5b226
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-01 14:04:16 +0300

    some fixes

--- src/components/Carousel/data.js
+++ src/components/Carousel/data.js
@@ -4,5 +4,5 @@ import image3 from "./images/image 3.png";
 import image4 from "./images/image 4.png";
 import image5 from "./images/image 5.png";
 import image6 from "./images/image 6.png";
-let data = [image1, image2, image3, image4, image5, image6];
-export default data;
+let Data = [image1, image2, image3, image4, image5, image6];
+export default Data;
