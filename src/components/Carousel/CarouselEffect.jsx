11e789206f51b7c05766862a2c48ea96104dd095
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-01 14:09:49 +0300

    data import updated

--- src/components/Carousel/CarouselEffect.jsx
+++ src/components/Carousel/CarouselEffect.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import Data from "./Data";
+import data from "./Data";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import css from "./CarouselEffect.module.css";
 const CarouselEffect = () => {
@@ -13,7 +13,7 @@ const CarouselEffect = () => {
         showIndicators={false}
         interval={2000}
       >
-        {Data.map((image, index) => (
+        {data.map((image, index) => (
           <img src={image} key={index} alt={`image ${index + 1}`} />
         ))}
       </Carousel>
